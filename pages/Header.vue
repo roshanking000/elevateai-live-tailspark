8433b48e9623cbeb71cef7bf4cf0cd0463c6245c
CryptoRoshan <118432414+tinyphoniex@users.noreply.github.com>
2024-01-30 03:57:17 +0900

    add 2 section in landing page

---
+++ pages/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <section
+    class="max-[991px]:bg-[linear-gradient(270deg,_rgb(7,_9,_20),_rgba(7,_9,_20,_0))] w-screen max-w-[100vw] border-b border-solid border-b-[#d9d9d90d] flex-col flex fixed top-0 left-0 right-0 bottom-auto z-[1000] bg-[rgb(221,_221,_221)] py-6 lg:py-9 px-5 md:px-10"
+    style="background-color: rgba(221, 221, 221, 0)"
+  >
+    <div
+      class="w-full max-w-7xl auto-cols-auto justify-stretch items-center mx-auto grid gap-0 grid-cols-[auto_max-content] lg:grid-cols-[176px_auto]"
+    >
+      <a
+        href="https://elevateai-live.webflow.io/"
+        class="max-[767px]: max-[991px]:mr-auto w-full max-w-[144px] float-left text-[#333333] relative"
+      >
+        <img
+          src="https://assets-global.website-files.com/657856d0336ed52d94d3834d/659ffd86a0aeacb3785fbfba_elevate-ai-logo.svg"
+          alt=""
+          class="relative max-[767px]:absolute w-full h-full inset-0 inline-block object-cover md:object-contain"
+        />
+      </a>
+      <nav
+        class="max-[991px]:hidden max-[991px]:text-left max-[991px]:bg-[linear-gradient(270deg,_rgb(7,_9,_20),_rgba(7,_9,_20,_0))] max-[991px]:flex-col max-[991px]: flex float-right relative items-end lg:items-center [@media(max-width:1065px)]:gap-6 gap-0 lg:gap-11 max-[991px]:pt-8 max-[991px]:pb-16 justify-end lg:justify-end"
+      >
+        <a
+          href="https://elevateai-live.webflow.io/pricing"
+          class="max-[991px]:block text-white uppercase font-bold transition hover:text-[#7a3eb4] hover:outline-0 max-[767px]:px-5 max-[991px]:pt-8 max-[991px]:px-10 text-base lg:text-sm"
+          >Pricing</a
+        >
+        <a
+          href="https://elevateai-live.webflow.io/about-us"
+          class="max-[991px]:block text-white uppercase font-bold transition hover:text-[#7a3eb4] hover:outline-0 max-[767px]:px-5 max-[991px]:pt-8 max-[991px]:px-10 text-base lg:text-sm"
+          >About Us</a
+        >
+        <a
+          href="https://elevateai-live.webflow.io/contact"
+          class="max-[991px]:block text-white uppercase font-bold transition hover:text-[#7a3eb4] hover:outline-0 max-[767px]:px-5 max-[991px]:pt-8 max-[991px]:px-10 text-base lg:text-sm"
+          >Contact</a
+        >
+        <a
+          href="https://elevateai-live.webflow.io/sign-up"
+          class="max-[991px]:block text-white uppercase font-bold transition hover:text-[#7a3eb4] hover:outline-0 max-[767px]:px-5 max-[991px]:pt-8 max-[991px]:px-10 text-base lg:text-sm"
+          >Sign Up</a
+        >
+        <a
+          href="https://webflow.com/made-in-webflow/website/elevateai-live"
+          target="_blank"
+          class="max-[991px]:block text-white uppercase font-bold transition hover:text-[#7a3eb4] hover:outline-0 max-[767px]:px-5 max-[991px]:pt-8 max-[991px]:px-10 text-base lg:text-sm"
+          >Clone template</a
+        >
+        <a
+          href="https://www.flowspark.co/"
+          target="_blank"
+          class="max-[767px]:ml-5 max-[991px]:mt-8 max-[991px]:ml-10 text-white text-center bg-[#472cb53d] border border-solid border-[#472cb5] px-6 font-semibold transition hover:bg-[#792CB524] hover:border-[#792cb5] inline-block py-5 rounded-2xl"
+          >Unlimited Webflow Dev</a
+        >
+      </nav>
+      <div
+        class="max-[991px]:block max-[991px]:text-white max-[991px]:-mr-3 float-right cursor-pointer select-none hidden relative text-2xl p-3 lg:p-[18px]"
+        style="-webkit-user-select: text"
+        tabindex="0"
+      >
+        <div>
+          <svg
+            width="1.25rem"
+            height="1rem"
+            viewBox="0 0 20 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="overflow-hidden"
+          >
+            <path
+              d="M19 7H1C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9H19C19.5523 9 20 8.55228 20 8C20 7.44772 19.5523 7 19 7Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M19 0H7C6.44772 0 6 0.447715 6 1C6 1.55228 6.44772 2 7 2H19C19.5523 2 20 1.55228 20 1C20 0.447715 19.5523 0 19 0Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M19 14H11C10.4477 14 10 14.4477 10 15C10 15.5523 10.4477 16 11 16H19C19.5523 16 20 15.5523 20 15C20 14.4477 19.5523 14 19 14Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,261 @@
 <template>
-	<div>
-	</div>
+  <div class="overflow-clip" style="background-color: rgb(15, 13, 31)">
+    <div
+      class="block fixed top-[0%] left-[0%] right-[auto] bottom-[auto]"
+    ></div>
+    <Header />
+    <main class="text-white bg-[#00000000] bg-none bg-auto relative block">
+      <header
+        class="max-[479px]:bg-[100%_100%] max-[767px]:bg-[100%_100%] max-[767px]:bg-contain font-normal relative block"
+      >
+        <div
+          class="max-[767px]:pl-5 max-[767px]:pr-5 pl-10 pr-10"
+          style="background-color: rgb(15, 13, 31)"
+        >
+          <div class="w-full max-w-7xl ml-auto mr-auto">
+            <div
+              class="max-[479px]:pb-40 max-[767px]:pt-40 max-[767px]:pb-48 max-[991px]:pt-48 max-[991px]:pb-48 pt-60 pb-[22rem]"
+            >
+              <section
+                class="z-10 w-full max-w-[952px] gap-6 flex-col flex"
+                data-aos="fade-up"
+                data-aos-duration="700"
+              >
+                <h1
+                  class="text-transparent bg-[linear-gradient(1deg,_rgba(65,_111,_166,_0.7),_rgb(255,_255,_255)_52%)] bg-clip-text font-bold text-4xl md:text-6xl"
+                >
+                  ElevateAI: Propel Your Marketing with the Power of AI
+                </h1>
+                <div class="max-w-[640px]">
+                  <p class="text-[#c4cff5]">
+                    Unleash AI to drive your marketing growth. Welcome to
+                    ElevateAI - where we convert raw data into strategic power.
+                  </p>
+                </div>
+                <div class="max-[479px]: max-[767px]:m-5 mt-6 lg:mt-8">
+                  <a
+                    color="primary"
+                    href="#"
+                    class="p-[1.4px] relative overflow-hidden max-w-full inline-block rounded-2xl bg-[linear-gradient(to_right,_#452bb1,_#1d1549_53%,_#452bb1)] hover:outline-0"
+                  >
+                    <div
+                      class="text-center bg-[radial-gradient(circle,_rgb(26,_20,_64)_56%,_rgb(29,_21,_73))] justify-center items-center px-6 py-5 flex rounded-2xl bg-[#0f0d1f]"
+                    >
+                      <p
+                        class="bg-[linear-gradient(to_right,_#c4cff5,_#fcfce7_47%,_#c4cff5)] text-transparent bg-clip-text font-bold"
+                      >
+                        Elevate Your Business
+                      </p>
+                    </div>
+                    <div
+                      class="w-10 h-10 bg-[#c4cff5] filter blur-[60px] absolute top-0 left-0 right-auto bottom-auto"
+                      style="
+                        will-change: transform;
+                        transform: translate3d(174.945%, 24.975%, 0px)
+                          scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg)
+                          rotateZ(0deg) skew(0deg, 0deg);
+                        transform-style: preserve-3d;
+                        opacity: 0;
+                      "
+                    ></div>
+                  </a>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+        <div
+          class="max-[479px]:max-w-[62rem] max-[479px]:top-[25.1rem] max-[479px]:-right-[16.1rem] w-full max-w-[65rem] mix-blend-overlay absolute top-[28.6rem] left-auto -right-[17rem]"
+        >
+          <img
+            src="https://assets-global.website-files.com/657856d0336ed52d94d3834d/65a0031d900156f1e7e851cc_hero-globe.svg"
+            loading="eager"
+            alt="Background Sphere"
+            class="object-contain relative max-[767px]:object-cover max-[767px]:absolute w-full h-full inset-[0%] max-w-full inline-block"
+          />
+        </div>
+        <div
+          class="max-[767px]:w-72 max-[767px]:bottom-[0%] max-[767px]:right-[0%] pointer-events-none absolute top-[auto] left-[auto] right-[10%] bottom-[-53%]"
+        >
+          <div class="w-full filter blur-[300px] mix-blend-color-dodge">
+            <img
+              src="https://assets-global.website-files.com/657856d0336ed52d94d3834d/65a00dd207047d6f84b90ddc_hero-gradient-svg.svg"
+              loading="lazy"
+              alt=""
+              class="object-contain relative max-[767px]:object-cover max-[767px]:absolute w-full h-full inset-[0%] max-w-full inline-block"
+            />
+          </div>
+        </div>
+      </header>
+      <div
+        data-w-id="759fcbd7-57b2-d22c-10a7-7f2435b34866"
+        class="max-[767px]:h-auto max-[991px]:h-auto h-[1700px] overflow-visible"
+      >
+        <div class="max-[991px]:top-[0%] sticky top-[20vh] overflow-hidden">
+          <div class="max-[767px]:pl-5 max-[767px]:pr-5 pl-10 pr-10">
+            <div class="w-full max-w-7xl ml-auto mr-auto">
+              <div
+                class="max-[767px]:pt-16 max-[767px]:pb-16 max-[991px]:pt-24 max-[991px]:pb-24 pt-32 pb-32"
+              >
+                <div
+                  class="max-[479px]:gap-20 max-[767px]:items-start max-[991px]:gap-20 max-[991px]:flex-col max-[991px]:grid-rows-[auto_auto] max-[991px]:grid-cols-[1fr] max-[991px]: gap-20 flex-row grid-rows-[auto] grid-cols-[1fr_1fr] auto-cols-[1fr] items-center flex overflow-visible"
+                >
+                  <section
+                    class="max-[991px]:max-w-full w-full max-w-[560px] flex-none"
+                    data-aos="fade-up"
+                    data-aos-duration="700"
+                  >
+                    <h2 class="font-bold text-3xl md:text-5xl">
+                      Discover The Unique ElevateAI Difference
+                    </h2>
+                    <div class="max-[479px]:pb-6 md:pb-10 lg:pb-12"></div>
+                    <a
+                      color="primary"
+                      href="#"
+                      class="p-[1.4px] relative overflow-hidden max-w-full inline-block rounded-2xl"
+                    >
+                      <div
+                        class="text-center bg-[radial-gradient(circle,_rgb(26,_20,_64)_56%,_rgb(29,_21,_73))] justify-center items-center px-6 py-5 flex rounded-2xl"
+                      >
+                        <p
+                          class="bg-[linear-gradient(to_right,_#c4cff5,_#fcfce7_47%,_#c4cff5)] text-transparent bg-clip-text font-['Plus_Jakarta_Sans',_sans-serif] font-bold"
+                        >
+                          Unleash the Power of AI
+                        </p>
+                      </div>
+                      <div
+                        class="w-10 h-10 bg-[#c4cff5] filter blur-[60px] absolute top-0 left-0 right-auto bottom-auto"
+                        style="
+                          will-change: transform;
+                          transform: translate3d(175%, 25%, 0px)
+                            scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg)
+                            rotateZ(0deg) skew(0deg, 0deg);
+                          transform-style: preserve-3d;
+                          opacity: 0;
+                        "
+                      ></div>
+                    </a>
+                  </section>
+                  <section
+                    class="max-[991px]:flex-col gap-8 flex-row flex w-full lg:w-[300%]"
+                  >
+                    <div
+                      class="max-[991px]:max-w-none max-[991px]:bg-[#3323694d] w-full max-w-md bg-[#332369cc] flex-col flex-none justify-between items-start p-8 flex [box-shadow:rgba(0,_0,_0,_0.05)_0px_1px_12px] rounded-xl gap-0 lg:gap-8"
+                      style="
+                        will-change: background;
+                        background-color: rgba(57, 38, 128, 0.647);
+                      "
+                    >
+                      <div class="flex-col items-start flex gap-6 sm:gap-8">
+                        <img
+                          src="https://assets-global.website-files.com/657856d0336ed52d94d3834d/657856d0336ed52d94d38367_Group%2047.svg"
+                          alt="Features Icon 1"
+                          class="h-8 inline-block"
+                        />
+                        <h3 class="font-bold text-base md:text-2xl">
+                          Predictive Analytics
+                        </h3>
+                      </div>
+                      <div
+                        class="max-[479px]:flex-col flex gap-4 sm:gap-24 mt-8 sm:mt-12 lg:mt-16 items-start md:items-end"
+                      >
+                        <p class="font-bold text-xl md:text-2xl">01</p>
+                        <p class="text-sm">
+                          Empower your decisions with our AI-driven predictive
+                          analytics, foreseeing market trends that keep you a
+                          step ahead.
+                        </p>
+                      </div>
+                    </div>
+                    <div
+                      class="flex max-[991px]:max-w-none max-[991px]:bg-[#3323694d] w-full max-w-md bg-[#332369cc] flex-col flex-none justify-between items-start p-8 [box-shadow:rgba(0,_0,_0,_0.05)_0px_1px_12px] rounded-xl gap-0 lg:gap-8"
+                      style="
+                        will-change: background, transform;
+                        background-color: rgba(71, 44, 181, 0.3);
+                        transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                          rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                          skew(0deg, 0deg);
+                        transform-style: preserve-3d;
+                      "
+                    >
+                      <div class="flex-col items-start flex gap-6 sm:gap-8">
+                        <img
+                          src="https://assets-global.website-files.com/657856d0336ed52d94d3834d/657856d0336ed52d94d38366_Vector.svg"
+                          alt="Features Icon 2"
+                          class="h-8 inline-block"
+                        />
+                        <h3 class="font-bold text-base md:text-2xl">
+                          Customer Segmentation
+                        </h3>
+                      </div>
+                      <div
+                        class="max-[479px]:flex-col flex gap-4 sm:gap-24 mt-8 sm:mt-12 lg:mt-16 items-start md:items-end"
+                      >
+                        <p class="font-bold text-xl md:text-2xl">02</p>
+                        <p class="text-sm">
+                          Demystify your audience with our AI-enhanced customer
+                          segmentation, enabling you to connect deeper and
+                          resonate better.
+                        </p>
+                      </div>
+                    </div>
+                    <div
+                      class="max-[991px]:max-w-none max-[991px]:bg-[#3323694d] w-full max-w-md bg-[#332369cc] flex-col flex-none justify-between items-start p-8 flex [box-shadow:rgba(0,_0,_0,_0.05)_0px_1px_12px] rounded-xl gap-0 lg:gap-8"
+                      style="
+                        will-change: background, transform;
+                        background-color: rgba(71, 44, 181, 0.3);
+                        transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                          rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                          skew(0deg, 0deg);
+                        transform-style: preserve-3d;
+                      "
+                    >
+                      <div class="flex-col items-start flex gap-6 sm:gap-8">
+                        <img
+                          src="https://assets-global.website-files.com/657856d0336ed52d94d3834d/657856d0336ed52d94d3836d_Vector%20(3).svg"
+                          alt="Features Icon 3"
+                          class="h-8 inline-block"
+                        />
+                        <h3 class="font-bold text-base md:text-2xl">
+                          Insightful&nbsp;Actions
+                        </h3>
+                      </div>
+                      <div
+                        class="max-[479px]:flex-col flex gap-4 sm:gap-24 mt-8 sm:mt-12 lg:mt-16 items-start md:items-end"
+                      >
+                        <p class="font-bold text-xl md:text-2xl">03</p>
+                        <p class="text-sm">
+                          Empower your decisions with our AI-driven predictive
+                          analytics, foreseeing market trends that keep you a
+                          step ahead.
+                        </p>
+                      </div>
+                    </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
+
+<script>
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+
+  mounted() {
+    AOS.init({
+      once: true,
+    });
+  },
+};
+</script>
